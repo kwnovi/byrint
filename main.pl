2ddaa6d6484162156a414f28f63604da0b1a4ddf
Benjamin Denom <ben.denom@gmail.com>
2015-02-02 17:10:40 +0100

    Calcul des coordonnées

--- main.pl
+++ main.pl
@@ -21,16 +21,24 @@
 %% PASSE (GAUCHE):- 
 %% PASSE (DROITE):- 
 
-
 %% TROUVER UN DÉPART SUR UNE LIGNE (IE UNE LISTE)
 
-depart_ligne(d).
-depart_ligne([T|Q]):- depart_ligne(T); depart_ligne(Q). 
+depart_ligne(d, 0).
+depart_ligne([T|Q], X) :- 
+	(
+		depart_ligne(T, X2);
+		depart_ligne(Q, X2)
+	),
+	X is X2+1.
 
 %% OK 
 
 %% TROUVER UN DÉPART DANS UN LABYRINTHE (IE UN ENSEMBLE DE LISTES)
 
-depart([T|Q]):-depart_ligne([T|Q]).
-depart([T|Q]):- depart_ligne(T);depart_ligne(Q).
+depart([T|_], X, 0) :-
+	depart_ligne(T, X2),
+	X is X2-1.
 
+depart([_|Q], X, Y) :-
+	depart(Q, X, Y2),
+	Y is Y2+1.
