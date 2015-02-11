3f6c29a64976387bd778d04ecb2fafdc11f04cbc
kwnovi <k.wnovi@gmail.com>
2015-02-11 18:44:29 +0100

    maze

--- main.pl
+++ main.pl
@@ -1,44 +1,37 @@
-%% FORMALISME : 
-%%		- D : DÉPART 
-%% 		- P : PASSAGE 
-%%		- V : VISITÉ
-%%		- M : MUR 
-%% 		- A : ARRIVÉE 
+%% EXEMPLE
 
-%% EXEMPLE DE LABYRINTHE
-%% [ [D,P,M],
-%%   [M,P,P],
-%%   [M,M,A] ]
+%% M M M 
+%% M P D 
+%% A P M
 
-%% TRANSFORMER EN MUR TOUT LES ÉLÉMENTS DÉJÀ PASSÉS N'AYANT PAS DE VOISIN 'P' (SANS PRENDRE EN COMPTE LES DIAGONALES)
-%% TRUE : SI ON RENCONTRE A(,!)
-%% FALSE : SI L'ON RETOURNE SUR LE DÉPART ET QU'IL EST ENTOURÉ DE MURS 
+mur(1,1).
+mur(1,2).
+mur(1,3).
+mur(2,1).
+mur(3,3).
 
-%% PASSE(A).
+libre(2,3).
+libre(2,2).
+libre(3,2).
 
-%% PASSE (HAUT):- 
-%% PASSE (BAS):- 
-%% PASSE (GAUCHE):- 
-%% PASSE (DROITE):- 
+arrive(3,1).
 
-%% TROUVER UN DÉPART SUR UNE LIGNE (IE UNE LISTE)
+appartient(E,[E|_]).
+appartient(E,[_|Q]):-appartient(E,Q).
+		
+parcourir(X,Y,_,_):-arrive(X,Y),!.
+parcourir(X,Y,X1,Y1):-
+		libre(X,Y),
+		suivant(X,Y,X1,Y1).
 
-depart_ligne(d, 0).
-depart_ligne([T|Q], X) :- 
-	(
-		depart_ligne(T, X2);
-		depart_ligne(Q, X2)
-	),
-	X is X2+1.
+suivant(_,Y,_,Y1):- Y1 is Y+1. %% MONTER
+suivant(_,Y,_,Y1):- Y1 is Y-1. %% DESCENDRE
+suivant(X,_,X1,_):- X1 is X+1. %% ALLER À DROITE 
+suivant(X,_,X1,_):- X1 is X-1. %% ALLER À GAUCHE 
 
-%% OK 
 
-%% TROUVER UN DÉPART DANS UN LABYRINTHE (IE UN ENSEMBLE DE LISTES)
-
-depart([T|_], X, 0) :-
-	depart_ligne(T, X2),
-	X is X2-1.
-
-depart([_|Q], X, Y) :-
-	depart(Q, X, Y2),
-	Y is Y2+1.
+chercher(X,Y,_,_,_,_):-arrive(X,Y).
+chercher(X0,Y0,X,Y,Parcouru, Parcour):-
+		parcourir(X0,Y0,X1,Y1),
+		\+ appartient(libre(X1,Y1), Parcouru),
+		chercher(X1,Y1,X,Y,[libre(X1,Y1)|Parcouru],Parcour).
