e5f3d0b6b794af73c5923d658b858543d7879284
kwnovi <k.wnovi@gmail.com>
2015-02-13 08:17:18 +0100

    end search

--- main.pl
+++ main.pl
@@ -1,6 +1,37 @@
+%% FORMALISME : 
+%%		- D : DÉPART 
+%% 		- P : PASSAGE 
+%%		- V : VISITÉ
+%%		- M : MUR 
+%% 		- A : ARRIVÉE 
 
-%% EXEMPLE
+%% EXEMPLE DE LABYRINTHE
+%% [ [D,P,M],
+%%   [M,P,P],
+%%   [M,M,A] ]
 
+
+%% TROUVER UN DÉPART SUR UNE LIGNE (IE UNE LISTE)
+
+depart_ligne(d, 0).
+depart_ligne([T|Q], X) :- 
+	(
+		depart_ligne(T, X2);
+		depart_ligne(Q, X2)
+	),
+	X is X2+1.
+
+%% TROUVER UN DÉPART DANS UN LABYRINTHE (IE UN ENSEMBLE DE LISTES)
+
+depart([T|_], X, 0) :-
+	depart_ligne(T, X2),
+	X is X2-1.
+
+depart([_|Q], X, Y) :-
+	depart(Q, X, Y2),
+	Y is Y2+1.
+
+% EXEMPLE UTILISÉ
 %% M M M 
 %% M P D 
 %% A P M
@@ -11,9 +42,9 @@ mur(1,3).
 mur(2,1).
 mur(3,3).
 
-libre(2,3).
-libre(2,2).
-libre(3,2).
+mur(2,3).
+mur(2,2).
+mur(3,2).
 
 arrive(3,1).
 
@@ -38,48 +69,7 @@ chercher(X0,Y0,X,Y,Parcouru, Parcour):-
 		chercher(X1,Y1,X,Y,[libre(X1,Y1)|Parcouru],Parcour).
 
 
-%% FORMALISME : 
-%%		- D : DÉPART 
-%% 		- P : PASSAGE 
-%%		- V : VISITÉ
-%%		- M : MUR 
-%% 		- A : ARRIVÉE 
 
-%% EXEMPLE DE LABYRINTHE
-%% [ [D,P,M],
-%%   [M,P,P],
-%%   [M,M,A] ]
 
-%% TRANSFORMER EN MUR TOUT LES ÉLÉMENTS DÉJÀ PASSÉS N'AYANT PAS DE VOISIN 'P' (SANS PRENDRE EN COMPTE LES DIAGONALES)
-%% TRUE : SI ON RENCONTRE A(,!)
-%% FALSE : SI L'ON RETOURNE SUR LE DÉPART ET QU'IL EST ENTOURÉ DE init_faits 
 
-%% PASSE(A).
-
-%% PASSE (HAUT):- 
-%% PASSE (BAS):- 
-%% PASSE (GAUCHE):- 
-%% PASSE (DROITE):- 
-
-%% TROUVER UN DÉPART SUR UNE LIGNE (IE UNE LISTE)
-
-depart_ligne(d, 0).
-depart_ligne([T|Q], X) :- 
-	(
-		depart_ligne(T, X2);
-		depart_ligne(Q, X2)
-	),
-	X is X2+1.
-
-%% OK 
-
-%% TROUVER UN DÉPART DANS UN LABYRINTHE (IE UN ENSEMBLE DE LISTES)
-
-depart([T|_], X, 0) :-
-	depart_ligne(T, X2),
-	X is X2-1.
-
-depart([_|Q], X, Y) :-
-	depart(Q, X, Y2),
-	Y is Y2+1.
 
